9a5810810f095ee37abde7cd9c1a7f41fe0561fd
srinivaspradeep <e.srinivaspradeep@gmail.com>
2026-07-06 16:36:43 +0530

    Test Code Updated

--- src/pagescroll/TestDemo.java
+++ src/pagescroll/TestDemo.java
@@ -1,14 +1,20 @@
 package pagescroll;
 
-import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class TestDemo {
+	WebDriver driver;
   @Test
-  public void f() {
+  public void facebook() {
+	  driver.get("https://www.facebook.com");
   }
   @BeforeTest
   public void beforeTest() {
+	  driver = new ChromeDriver();
+	  driver.manage().window().maximize();
   }
 
 }

6aef6a70c017945ca981f4220e43ceb736f42023
srinivaspradeep <e.srinivaspradeep@gmail.com>
2026-07-07 17:55:16 +0530

    From Eclipse

--- src/locators/TueTestDemo.java
+++ src/locators/TueTestDemo.java
@@ -1,14 +1,20 @@
 package locators;
 
-import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class TueTestDemo {
+	WebDriver driver;
   @Test
-  public void f() {
+  public void google1() {
+	  driver.get("https://www.google.com/");
   }
   @BeforeTest
   public void beforeTest() {
+	  driver = new ChromeDriver();
+	  driver.manage().window().maximize();
   }
 
 }
